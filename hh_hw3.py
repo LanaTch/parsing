4e059f7a1cb1ac5bfcdeacb3716e431045cd9015
LanaTch <44633700+LanaTch@users.noreply.github.com>
2019-07-27 23:34:18 +0300

    Update hh_hw3.py

--- hh_hw3.py
+++ hh_hw3.py
@@ -13,7 +13,11 @@ from lxml import html
 from pprint import pprint
 import pandas as pd
 
+pd.options.display.max_columns = 4
+
 def return_html_from_url(url, param_hh):
+    # функция по url позвращает html страницы по запросу
+
     headers_hh = {'accept':'*/&*',
                'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 '
                              '(KHTML, like Gecko) Chrome/75.0.3770.142 Safari/537.36'}
@@ -26,6 +30,7 @@ def return_html_from_url(url, param_hh):
 
 
 def find_vacancies_headers(html_page):
+    # функция ищет в html заголовок с названием вакансии, ссылкой на нее и зарплатой
 
     v_ = html.fromstring(html_page).xpath('//a[contains(@class, "bloko-link HH-LinkModifier")]')
     s_ = html.fromstring(html_page).xpath\
@@ -36,6 +41,8 @@ def find_vacancies_headers(html_page):
 
 
 def find_last_page(html_page):
+    # функция ищет количество страниц с вакансиями (последнее поле в списке страниц)
+
     last_page_str = html.fromstring(html_page).xpath \
             ('//div[@data-qa="pager-block"]/*/a[@class="bloko-button HH-Pager-Control"]')
 
@@ -49,7 +56,7 @@ if __name__ == '__main__':
     search_vacancy = input('какую вакансию искать?  ')
 
     vacancies_list = []
-    last_page = 1
+    last_page = 1 # хотя бы 1 страница с вакансиями будет
     i = 0
     while i < last_page:
         param_search = {'text': search_vacancy,
@@ -59,8 +66,9 @@ if __name__ == '__main__':
         hh_html = return_html_from_url(f'https://hh.ru/search/vacancy', param_search)
 
         if i == 0:
-            last_page = find_last_page(hh_html)
-            print(f'всего {last_page} страниц с вакансиями "{search_vacancy}"')
+            max_pages = find_last_page(hh_html)
+            print(f'всего {max_pages} страниц с вакансиями "{search_vacancy}"')
+            last_page = int(input(f'сколько страниц просмотреть (максимум {max_pages})? '))
             print('подождите, идет поиск...')
 
         vacancies, salaries = find_vacancies_headers(hh_html)
@@ -76,7 +84,8 @@ if __name__ == '__main__':
 
         i+=1
 
-    pd.options.display.max_columns = 4
     print(f'найдено {len(vacancies_list)} вакансий')
+
     max_pos = int(input(f'сколько вакансий вывести на экран?   '))
+
     pprint(pd.DataFrame(vacancies_list[:max_pos]))
